8ef4448a1da1fb1291a1c95ff32a9890e92f9308
Zeenat <zeenatlawal82@gmail.com>
2025-09-30 14:00:20 +0100

    Fix notification

--- utils/context/NotificationProvider.tsx
+++ utils/context/NotificationProvider.tsx
@@ -253,7 +253,7 @@ export function NotificationProvider({
         responseListener.current.remove();
       }
     };
-  }, [userProfile?.userId]);
+  }, []);
 
   useEffect(() => {
     if (isAuthenticated && userProfile) {
